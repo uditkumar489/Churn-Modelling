f4dbba294a13834ca720af27d9b500d9e47501a4
udit kumar <uditkumar489@gmail.com>
2018-01-13 08:22:35 +0530

    libraries imported
    ..

---
+++ chModl.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 13 07:59:38 2018
+
+@author: Udit
+"""
+# Artificial Neural Network
+
+# IMPORTANT
+# Please intall : {1.Theano  ,  2.Tensorflow  ,  3.Keras}  before executing it.
+
+
+"""
+Data PreProcessing
+"""
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
