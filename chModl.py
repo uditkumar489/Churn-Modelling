be6499c60cbb059541ba51acd93919eaa3597dba
udit kumar <uditkumar489@gmail.com>
2018-01-13 08:29:49 +0530

    data preProcessed
    encoding , splitting , scalling DONE !!

--- chModl.py
+++ chModl.py
@@ -22,3 +22,23 @@ import pandas as pd
 dataset = pd.read_csv('Churn_Dataset.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
+
+# Categorical data Encoded
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
