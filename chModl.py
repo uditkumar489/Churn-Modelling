8d03c700bebb184a39d77c4b2fd9411ba0b0b8d5
udit kumar <uditkumar489@gmail.com>
2018-01-13 08:44:55 +0530

    Predictions @ Testing
    ..

--- chModl.py
+++ chModl.py
@@ -62,3 +62,16 @@ classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid')) 
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy']) #compiling
 
 classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100) #fitting to dataset
+
+
+
+"""
+Making Predictions and Testing
+"""
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# The Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
